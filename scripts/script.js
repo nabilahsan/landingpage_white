dc780a5f000e1968447c818d71ed723cd7b8b04b
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2018-06-30 16:04:06 -0500

    try fixing scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
  
- var myVar = setTimeout(myTimer, 4000); //start running function after 4seconds
+ var myVar = setTimeout(myTimer, 1000); //start running function after 4seconds
  
  function myTimer(){
   var scroll = new SmoothScroll(); //using smoothscroll libraary
   var anchor = document.querySelector('#defn'); //where it is scrolling to
-  var toggle = document.querySelector('#space'); //where it is scrolling from
-  var options = {speed: 3000, easing: 'easeOutCubic', updateURL: false}; //speed: how long it scrolls, easing is the transition effect
+  var toggle = document.querySelector('#logo'); //where it is scrolling from
+  var options = {speed: 3000, easing: 'easeOutCubic', updateURL: false, clip: true}; //speed: how long it scrolls, easing is the transition effect
   scroll.animateScroll(anchor, toggle, options);
 }
