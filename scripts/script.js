010c9ae18e72d9d892f5977b501338da85627e82
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2018-06-30 14:08:44 -0500

    added text highlight and resize image

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
-//Animate scroll up.
-//Idiot proofing
-/*var myVar = setInterval(myTimer, 4000);
-
-function myTimer(){
-	var element = document.getElementById("defn");
-	element.scrollIntoView({behavior: "smooth", block: "end", alignTo: "false" });
-}*/
+ 
+ var myVar = setTimeout(myTimer, 4000); //start running function after 4seconds
+ 
+ function myTimer(){
+  var scroll = new SmoothScroll(); //using smoothscroll libraary
+  var anchor = document.querySelector('#defn'); //where it is scrolling to
+  var toggle = document.querySelector('#space'); //where it is scrolling from
+  var options = {speed: 3000, easing: 'easeOutCubic', updateURL: false}; //speed: how long it scrolls, easing is the transition effect
+  scroll.animateScroll(anchor, toggle, options);
+}
